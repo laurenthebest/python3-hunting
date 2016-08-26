d1f482bfac7793136a85c338ac5f0d5c108d4238
Siyu Lu <siyulu@Siyus-MacBook.local>
2016-08-25 23:53:59 -0400

    add first file

---
+++ first.py
@@ -0,0 +1,20 @@
+#total = 1 + 3 +5 +7 + 9
+#total2 = 2 + 4 +6 +8 +10
+#print ("1 + 3+ 5 +7 + 9 =", total)
+#print ("2 + 4 + 6 +8 +10 =",total2)
+#print ("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10=",total+total2)
+
+print ("1+2=", 1+2)
+print ("1-2=",1-2)
+print("1*2=",1*2)
+print("1/2=",1/2)
+
+def pt():
+	print("")
+
+
+
+for i in range(100):
+	pt()
+
+
